miceno/atom

commit 676305ba025d027dfa8b3af214775ee842a1fe21

Add DO option to clipboard export, refs #13395
Add a new system setting to enable export of digital objects attached to
the clipboard (information object and actor records only). All digital
object types can be exported, but only masters are exported for any
image type.

Digital objects are included in the export ZIP along with associated
metadata.

Incorporate basic user permissions checks prior to export, with further
checks for rights as part of the background job/task. Add a new method
to the digital object model to check for any conditional copyright
attached. If set, then do not export the affected digital object.

Modify the export page interface to rebuild options panel depending on
controller-based permissions checks. Also move help and other message
construction from template to controller.

Make the selection of descendant or digital objects in the export page
mutually exclusive. Add several new styling rules (LESS) to the Dominium
theme in scaffolding that apply to the options panel (#export-options).

Change some help and alert messages to improve consistency.

Build additional export form parameter validation in the controller to
mitigate attempts to manually override parameters, and also to prevent
direct access to the export page URL without objects on the clipboard.

apps/qubit/modules/digitalobject/actions/viewAction.class.php
@@ -81,40 +81,9 @@ class DigitalObjectViewAction extends sfAction
       return false;
     }
 
-    // Only if the copyright statement is enabled
-    if ('1' !== sfConfig::get('app_digitalobject_copyright_statement_enabled', false))
+    if ($this->resource->hasConditionalCopyright())
     {
-      return false;
-    }
-
-    // Check if there is any right statement associated with the object where
-    // the basis = copyright and the restriction = conditional (regardless of
-    // the Rights Act). We don't need to show the popup otherwise.
-    $sql = 'SELECT EXISTS(
-      SELECT 1
-        FROM '.QubitObject::TABLE_NAME.' o
-        JOIN '.QubitRelation::TABLE_NAME.' rel ON (rel.subject_id = o.id)
-        JOIN '.QubitGrantedRight::TABLE_NAME.' gr ON (rel.object_id = gr.rights_id)
-        JOIN '.QubitRights::TABLE_NAME.' r ON (gr.rights_id = r.id)
-      WHERE
-        o.id = ? AND
-        rel.type_id = ? AND
-        gr.restriction = ? AND
-        r.basis_id = ?
-      LIMIT 1) AS has';
-    $r = QubitPdo::fetchOne($sql, array(
-      $this->resource->object->id,
-      QubitTerm::RIGHT_ID,
-      QubitGrantedRight::CONDITIONAL_RIGHT,
-      QubitTerm::RIGHT_BASIS_COPYRIGHT_ID));
-
-    if (false === $r || !isset($r->has))
-    {
-      throw new sfException('Unexpected error');
-    }
-    if ('1' !== $r->has)
-    {
-      return false;
+      return true;
     }
 
     return false === $this->isAccessTokenValid();

lib/model/QubitDigitalObject.php
@@ -3416,4 +3416,61 @@ class QubitDigitalObject extends BaseDigitalObject
 
     return $size > sfConfig::get('app_upload_limit', 0) * pow(1024, 3);
   }
+
+  /**
+   * Check if digital object (master) has an active conditional copyright statement
+   *
+   * @return boolean
+   */
+  public function hasConditionalCopyright()
+  {
+    // Only if this is a master image and copyright statement is enabled
+    if (QubitTerm::MASTER_ID != $this->usageId || !sfConfig::get('app_digitalobject_copyright_statement_enabled', false))
+    {
+      return false;
+    }
+
+    if (isset($this->object))
+    {
+      $object = $this->object;
+    }
+    else if (isset($this->parent))
+    {
+      $object = $this->parent->object;
+    }
+
+    if (!isset($object))
+    {
+      throw new sfException('Couldn\'t find related object for digital object');
+    }
+
+    // Check if there is any rights statement associated with the object where
+    // the basis = copyright and the restriction = conditional (regardless of
+    // the Rights Act).
+    $sql = 'SELECT EXISTS(
+      SELECT 1
+        FROM '.QubitObject::TABLE_NAME.' o
+        JOIN '.QubitRelation::TABLE_NAME.' rel ON (rel.subject_id = o.id)
+        JOIN '.QubitGrantedRight::TABLE_NAME.' gr ON (rel.object_id = gr.rights_id)
+        JOIN '.QubitRights::TABLE_NAME.' r ON (gr.rights_id = r.id)
+      WHERE
+        o.id = ? AND
+        rel.type_id = ? AND
+        gr.restriction = ? AND
+        r.basis_id = ?
+      LIMIT 1) AS has';
+    $r = QubitPdo::fetchOne($sql, array(
+        $object->id,
+        QubitTerm::RIGHT_ID,
+        QubitGrantedRight::CONDITIONAL_RIGHT,
+        QubitTerm::RIGHT_BASIS_COPYRIGHT_ID));
+
+    if (false === $r || !isset($r->has))
+    {
+      // An unexpected problem has stopped the query, return true as default
+      return true;
+    }
+
+    return $r->has;
+  }
 }

apps/qubit/modules/default/actions/alertsComponent.class.php
@@ -43,22 +43,34 @@ class DefaultAlertsComponent extends sfComponent
         if (isset($firstJobAlertSkipped[$job->id]))
         {
           // Assemble job description
-          $message = $this->context->i18n->__('%1% (started: %2%, status: %3%).', array(
-            '%1%' => (string)$job,
-            '%2%' => $job->getCreationDateString(),
-            '%3%' => $job->getStatusString()));
+          $message = $this->context->i18n->__('%1% (started: %2%, status: %3%).', 
+            array(
+              '%1%' => (string)$job,
+              '%2%' => $job->getCreationDateString(),
+              '%3%' => $job->getStatusString()
+            )
+          );
 
           // Add download path. if applicable
           if (isset($job->downloadPath) && $job->statusId == QubitTerm::JOB_STATUS_COMPLETED_ID)
           {
-            $message .= $this->context->i18n->__(' %1%Download%2% (%3% b)', array(
-              '%1%' => sprintf('<a href="%s">', sfConfig::get('app_siteBaseUrl') .'/'. $job->downloadPath),
-              '%2%' => '</a>',
-              '%3%' => hr_filesize(filesize($job->downloadPath))));
+            $message .= $this->context->i18n->__(' %1%Download%2% (%3% b)', 
+              array(
+                '%1%' => sprintf('<a href="%s">', sfConfig::get('app_siteBaseUrl') .'/'. $job->downloadPath),
+                '%2%' => '</a>',
+                '%3%' => hr_filesize(filesize($job->downloadPath))
+              )
+            );
           }
-
-          // Add refresh link
-          $message .= ' &mdash; <a href="javascript:location.reload();">refresh the page</a> for updates.';
+          else
+          {
+            $message .= ' ' . $this->context->i18n->__('%1%Refresh the page%2% for progress updates.', 
+              array(
+                '%1%' => '<a href="javascript:location.reload();">',
+                '%2%' => '</a>'
+              )
+            );
+          }
 
           // Determine alert type to show
           $alertTypes = array(

apps/qubit/modules/object/actions/exportAction.class.php
@@ -23,30 +23,113 @@ class ObjectExportAction extends DefaultEditAction
   public static
     $NAMES = array(
       'levels',
-      'objectType',
-      'format');
+      'type',
+      'format',
+      'includeDescendants',
+      'includeAllLevels',
+      'includeDigitalObjects',
+      'includeDrafts');
 
   private $choices = array();
 
   protected function earlyExecute()
   {
+    sfProjectConfiguration::getActive()->loadHelpers(array('I18N'));
+
+    // Initialize help array: messages added depending on visibility of fields
+    $this->helpMessages = array();
+
+    $this->typeChoices = array(
+      'informationObject' => sfConfig::get('app_ui_label_informationobject'),
+      'actor' => sfConfig::get('app_ui_label_actor'),
+      'repository' => sfConfig::get('app_ui_label_repository')
+    );
+
     $this->form->getValidatorSchema()->setOption('allow_extra_fields', true);
   }
 
   public function execute($request)
   {
-    $this->format = $request->getParameter('format', 'csv');
-    $this->objectType = $request->getParameter('objectType');
+
+    // Get object type and validate
+    // Currently 'switch' to process the inbound parameter, validate and set default - could be if/then if preferred
+    $this->objectType = trim(strtolower($request->getParameter('type')));
+    switch ($this->objectType)
+    {
+      case 'actor':
+        $className = 'QubitActor';
+
+        break;
+
+      case 'repository':
+        $className = 'QubitRepository';
+
+        break;
+
+      default:
+        $this->objectType = 'informationObject';
+        $className = 'QubitInformationObject';
+    }
+
+    // Stop direct access to export url or empty export attempts
+    // Check that the user has something on the clipboard for the current type - otherwise redirect back to clipboard
+    $items = $this->context->user->getClipboard()->countByType();
+    if(empty($items) || !array_key_exists($className, $items) || 1 > $items[$className])
+    {
+      $this->redirect($this->context->routing->generate(null, array('module' => 'user', 'action' => 'clipboard')) . '?type=' . $this->objectType);
+    }
+
+    // Get format and validate
+    // Currently 'switch' to process the inbound parameter, validate and set default - could be if/then if preferred
+    $this->formatType = trim(strtolower($request->getParameter('format')));
+    if(!('xml' == $this->formatType || 'csv' == $this->formatType) || 'repository' == $this->objectType)
+    {
+        $this->formatType = 'csv';
+    }
+
+    // Basic permission check to determine whether digital object export should be made available
+    $this->digitalObjectsAvailable = false;
+    if (sfConfig::get('app_clipboard_export_digitalobjects_enabled', false) && ('informationObject' == $this->objectType || ('actor' == $this->objectType && $this->context->user->isAuthenticated())))
+    {
+      $this->digitalObjectsAvailable = true;
+    }
+
+    // Show export options panel if:
+    // information object type
+    // or, if actor type and digital objects are on the clipboard
+    $this->showOptions = 'informationObject' == $this->objectType || ('actor' == $this->objectType && $this->digitalObjectsAvailable);
+
+    // Get field includeDescendants if:
+    // options enabled
+    // and, information object type
+    $this->descendantsIncluded = $this->showOptions && 'informationObject' == $this->objectType && 'on' == $request->getParameter('includeDescendants');
+
+    // get field includeAllLevels if:
+    // descendantsIncluded enabled
+    $this->descendantsAllLevels = $this->descendantsIncluded && 'on' == $request->getParameter('includeAllLevels');
+
+    // Get field includeDigitalObjects if:
+    // digital objects are on the clipboard
+    // and, information object type and descendants are not selected
+    // or, if actor type
+    $this->includeDigitalObjects = false;
+    if ($this->digitalObjectsAvailable)
+    {
+      $this->includeDigitalObjects = 'on' == $request->getParameter('includeDigitalObjects');
+    }
+
+    // Get field includeDrafts if:
+    // options enabled
+    // and, user is authenticated
+    $this->draftsIncluded = false;
+    if ($this->showOptions && $this->context->user->isAuthenticated())
+    {
+      $this->draftsIncluded = 'on' == $request->getParameter('includeDrafts');
+    }
 
     parent::execute($request);
 
     $this->response->addJavaScript('exportOptions', 'last');
-
-    $this->redirectUrl = array('module' => 'object', 'action' => 'export');
-    if (null !== $referrer = $request->getReferer())
-    {
-      $this->redirectUrl = $referrer;
-    }
 
     $this->title = $this->context->i18n->__('Export');
 
@@ -60,7 +143,7 @@ class ObjectExportAction extends DefaultEditAction
 
         $this->doBackgroundExport($request);
 
-        $this->redirect($this->redirectUrl);
+        $this->redirect($this->context->routing->generate(null, array('module' => 'user', 'action' => 'clipboard')) . '?type=' . $this->objectType);
       }
     }
   }
@@ -69,54 +152,122 @@ class ObjectExportAction extends DefaultEditAction
   {
     switch ($name)
     {
-      case 'levels':
-        $this->form->setValidator('levels', new sfValidatorPass);
+      case 'type':
+        $this->form->setValidator('type', new sfValidatorString(array('required' => true)));
+        $this->form->setWidget('type', new sfWidgetFormSelect(array('label' => __('Type'), 'choices' => $this->typeChoices)));
+        $this->form->setDefault('type', $this->objectType);
 
-        foreach (QubitTerm::getLevelsOfDescription() as $item)
+        break;
+
+      case 'format':
+        $this->form->setValidator('format', new sfValidatorString(array('required' => true)));
+        $choices = array();
+        $choices['csv'] = $this->context->i18n->__('CSV');
+        if('repository' != $this->objectType)
         {
-          $this->choices[$item->id] = $item->__toString();
+          $choices['xml'] = $this->context->i18n->__('XML');
+        }
+        $this->form->setWidget('format', new sfWidgetFormSelect(array('label' => __('Format'), 'choices' => $choices)));
+        $this->form->setDefault('format', 'actor' != $this->objectType ? 'xml' : 'csv');
+
+        break;
+
+      // Enable field includeDescendants if:
+      // options enabled
+      // and, information object type
+      case 'includeDescendants':
+        if ($this->showOptions && 'informationObject' == $this->objectType)
+        {
+          $this->form->setWidget('includeDescendants', new sfWidgetFormInputCheckbox(array('label' => __('Include descendants'))));
+          $this->form->setDefault('includeDescendants', false);
+          $this->helpMessages[] = __('Choosing "Include descendants" will include all lower-level records beneath those currently on the clipboard in the export.');
         }
 
-        $size = count($this->choices);
+        break;
+
+      // Enable field includeAllLevels if:
+      // options enabled
+      // and, information object type
+      case 'includeAllLevels':
+        if ($this->showOptions && 'informationObject' == $this->objectType)
+        {
+          $this->form->setWidget('includeAllLevels', new sfWidgetFormInputCheckbox(array('label' => __('Include all descendant levels of description'))));
+          $this->form->setDefault('includeAllLevels', true);
+        }
+
+        break;
+
+      // Enable field levels if:
+      // options enabled
+      // and, information object type
+      case 'levels':
+
+        $this->form->setValidator('levels', new sfValidatorPass);
+
+        $this->levelChoices = array();
+        foreach (QubitTerm::getLevelsOfDescription() as $item)
+        {
+          $this->levelChoices[$item->id] = $item->__toString();
+        }
+
+        $size = count($this->levelChoices);
         if ($size === 0)
         {
           $size = 4;
         }
 
-        $this->form->setWidget('levels', new sfWidgetFormSelect(array('choices' => $this->choices, 'multiple' => true), array('size' => $size)));
+        if ($this->showOptions && 'informationObject' == $this->objectType)
+        {
+          $this->form->setWidget('levels', new sfWidgetFormSelect(
+            array(
+              'label' => __('Select levels of descendant descriptions for inclusion'), 
+              'help' => __('If no levels are selected, the export will fail. You can use the control (Mac ⌘) and/or shift keys to multi-select values from the Levels of description menu. ' . 
+                           'It is necessary to include the level(s) above the desired export level, up to and including the level contained in the clipboard. ' . 
+                           'Otherwise, no records will be included in the export.'), 
+              'choices' => $this->levelChoices, 
+              'multiple' => true
+            ), 
+            array(
+              'size' => $size
+            )
+          ));
+        }
+        
+        break;
+
+      // Enable field includeDigitalObjects if:
+      // digital objects are available
+      case 'includeDigitalObjects':
+        if ($this->digitalObjectsAvailable)
+        {
+          switch ($this->objectType)
+          {
+            case 'informationObject':
+              $this->helpMessages[] = __('Note: Digital objects with restricted access or copyright will not be exported.') . ' ' . __('It is not possible to select both digital objects and descendants for export at the same time. Digital objects can only be exported for records that are on the clipboard.');
+
+              break;
+
+            case 'actor':
+              $this->helpMessages[] = __('Note: Digital objects with restricted access or copyright will not be exported.');
+
+              break;
+          }
+          $this->form->setWidget('includeDigitalObjects', new sfWidgetFormInputCheckbox(array('label' => __('Include digital objects'))));
+          $this->form->setDefault('includeDigitalObjects', true);
+        }
 
         break;
 
-      case 'objectType':
-        if (isset($this->objectType))
+      // Enable field includeDrafts if:
+      // options enabled
+      // and, user is authenticated
+      case 'includeDrafts':
+        if ($this->showOptions && $this->context->user->isAuthenticated())
         {
-          $choices = array(
-            $this->objectType => sfConfig::get('app_ui_label_'.strtolower($this->objectType))
-          );
+          $this->form->setWidget('includeDrafts', new sfWidgetFormInputCheckbox(array('label' => __('Include draft records'))));
+          $this->helpMessages[] = __('Choosing "Include draft records" will include those marked with a Draft publication status in the export. Note: if you do NOT choose this option, any descendants of a draft record will also be excluded, even if they are published.');
+          $this->form->setDefault('includeDrafts', true);
         }
-        else
-        {
-          $choices = array(
-            'informationObject' => sfConfig::get('app_ui_label_informationobject'),
-            'actor' => sfConfig::get('app_ui_label_actor'),
-            'repository' => sfConfig::get('app_ui_label_repository')
-          );
-        }
-
-        $this->form->setValidator('objectType', new sfValidatorString(array('required' => true)));
-        $this->form->setWidget('objectType', new sfWidgetFormSelect(array('choices' => $choices)));
-
-        break;
-
-      case 'format':
-        $choices = array(
-          'csv' => $this->context->i18n->__('CSV'),
-          'xml' => $this->context->i18n->__('XML')
-        );
-
-        $this->form->setDefault('format', $this->format);
-        $this->form->setValidator('format', new sfValidatorString(array('required' => true)));
-        $this->form->setWidget('format', new sfWidgetFormSelect(array('choices' => $choices)));
 
         break;
 
@@ -139,7 +290,7 @@ class ObjectExportAction extends DefaultEditAction
 
         break;
 
-      case 'objectType':
+      case 'type':
       case 'format':
         $this->$name = $this->form->getValue($name);
 
@@ -152,8 +303,9 @@ class ObjectExportAction extends DefaultEditAction
 
   protected function doBackgroundExport($request)
   {
-    // Create array of selections to pass to background job where Term ID will
-    // be key, and Term description is value.
+    // Create array of selections to pass to background job where 
+    // Term ID will be key, and Term description is value
+    $levelsOfDescription = array();
     foreach ($this->levels as $value)
     {
       $levelsOfDescription[$value] = $this->choices[$value];
@@ -161,20 +313,27 @@ class ObjectExportAction extends DefaultEditAction
 
     $options = array(
       'params' => array('fromClipboard' => true, 'slugs' => $this->context->user->getClipboard()->getAll()),
-      'current-level-only' => 'on' !== $request->getParameter('includeDescendants'),
-      'public' => 'on' !== $request->getParameter('includeDrafts'),
+      'current-level-only' => !$this->descendantsIncluded,
+      'public' => !$this->draftsIncluded,
       'objectType' => $this->objectType,
-      'levels' => $levelsOfDescription,
-      'name' => $this->context->i18n->__('CSV export')
+      'levels' => $levelsOfDescription
     );
 
-    if ('xml' === $this->format)
+    $options['name'] = $this->context->i18n->__('xml' === $this->formatType ? 'XML export' : 'CSV export' );
+
+    if($this->includeDigitalObjects)
     {
-      $options['name'] = $this->context->i18n->__('XML export');
+      $options['name'] = $this->context->i18n->__('%1% and %2%', 
+        array(
+          '%1%' => sfConfig::get('app_ui_label_digitalobject'),
+          '%2%' => $options['name']
+        )
+      );
+      $options['includeDigitalObjects'] = true;
     }
 
-    // When exporting actors, ensure aliases and relations are also exported.
-    if ('actor' === $this->objectType && 'csv' === $this->format)
+    // When exporting actors, ensure aliases and relations are also exported
+    if ('actor' === $this->objectType && 'csv' === $this->formatType)
     {
       $options['aliases'] = true;
       $options['relations'] = true;
@@ -182,16 +341,24 @@ class ObjectExportAction extends DefaultEditAction
 
     try
     {
-      // Preview export before attempting, if job supports this
-      $exportPreviewSupported = method_exists($this->getJobNameString(), 'exportOrCheckForResults');
+      $jobName = $this->getJobNameString();
 
-      if ($exportPreviewSupported && !call_user_func(array($this->getJobNameString(), 'exportOrCheckForResults'), $options))
+      // Preview export before attempting, if job supports this
+      $exportPreviewSupported = method_exists($jobName, 'exportOrCheckForResults');
+
+      if ($exportPreviewSupported && !call_user_func(array($jobName, 'exportOrCheckForResults'), $options))
       {
-        throw new sfException('Export complete - no records were exported.');
+        throw new sfException($this->context->i18n->__(
+          'No records were exported for your current selection. Please %1%refresh the page and choose different export options%2%.', 
+          array(
+            '%1%' => '<a href="javascript:location.reload();">',
+            '%2%' => '</a>',
+          )
+        ));
       }
       else
       {
-        $job = QubitJob::runJob($this->getJobNameString(), $options);
+        $job = QubitJob::runJob($jobName, $options);
 
         // If anonymous user, store job ID in session
         if (!$this->context->user->isAuthenticated())
@@ -200,21 +367,29 @@ class ObjectExportAction extends DefaultEditAction
           $manager->addJobAssociation($job);
         }
 
-        if ($this->context->user->isAuthenticated())
-        {
-          $message = $this->context->i18n->__('%1%Export initiated.%2% Check %3%job management%4% page to download the results when it has completed.', array(
-            '%1%' => '<strong>',
-            '%2%' => '</strong>',
-            '%3%' => sprintf('<a href="%s">', $this->context->routing->generate(null, array('module' => 'jobs', 'action' => 'browse'))),
-            '%4%' => '</a>'));
-        }
-        else
-        {
-          $message = $this->context->i18n->__('Export initiated. Progress will be reported, and a download link provided, in a subsequent notification &mdash; <a href="javascript:location.reload();">refresh the page</a> for updates.');
-        }
-      }
+        $message = array();
 
-      $this->context->user->setFlash('notice', $message);
+        $message[] = $this->context->i18n->__(
+          '%1%Your %2% export package is being built.%3% ' . ($this->context->user->isAuthenticated() ? 'The %4%job management page%5% will show progress and a download link when complete.' : 'Please %4%refresh the page%5% to see progress and a download link when complete.'), 
+          array(
+            '%1%' => '<strong>',
+            '%2%' => strtolower($this->typeChoices[$this->objectType]),
+            '%3%' => '</strong>',
+            '%4%' => ($this->context->user->isAuthenticated() ? sprintf('<a href="%s">', $this->context->routing->generate(null, array('module' => 'jobs', 'action' => 'browse'))) : '<a href="javascript:location.reload();">') . '<strong>',
+            '%5%' => '</strong></a>'
+          )
+        );
+
+        $message[] = $this->context->i18n->__(
+          '%1%Note:%2% AtoM may remove export packages after a period of time to free up storage space. When your export is ready you should download it as soon as possible.', 
+          array(
+            '%1%' => '<strong>',
+            '%2%' => '</strong>'
+          )
+        );
+
+      }
+      $this->context->user->setFlash('info', '<p>' . implode('</p><p>', $message) . '</p>');
     }
     catch (sfException $e)
     {
@@ -228,7 +403,7 @@ class ObjectExportAction extends DefaultEditAction
     switch ($this->objectType)
     {
       case 'informationObject':
-        if ('csv' == $this->format)
+        if ('csv' == $this->formatType)
         {
           return 'arInformationObjectCsvExportJob';
         }
@@ -238,7 +413,7 @@ class ObjectExportAction extends DefaultEditAction
         }
 
       case 'actor':
-        if ('csv' == $this->format)
+        if ('csv' == $this->formatType)
         {
           return 'arActorCsvExportJob';
         }

commit 3dd938d2835b653d26b978b5cda4856957f7dcd0

Allow disabling copyright pop-up, refs #13399
Fix a regression introduced in commit 676305b that displayed the conditional
copyright pop-up whenever a "master" digital object is requested.

The error was introduced by inverting the `hasConditionalCopyright()` check to
return true on success, instead of returning false (thus avoiding the following
valid token check) when `hasConditionalCopyring()` returned false.

apps/qubit/modules/digitalobject/actions/viewAction.class.php
@@ -75,17 +75,14 @@ class DigitalObjectViewAction extends sfAction
 
   protected function needsPopup($action)
   {
-    // Only if the user is reading the master digital object
-    if ($action !== 'readMaster')
+    // Only if the user is reading the master digital object, and the resource
+    // has a PREMIS conditional copyright restriction
+    if ($action != 'readMaster' || !$this->resource->hasConditionalCopyright())
     {
       return false;
     }
 
-    if ($this->resource->hasConditionalCopyright())
-    {
-      return true;
-    }
-
+    // Show the pop-up if a valid access token was not submitted
     return false === $this->isAccessTokenValid();
   }
 

lib/model/QubitDigitalObject.php
@@ -3425,8 +3425,9 @@ class QubitDigitalObject extends BaseDigitalObject
   public function hasConditionalCopyright()
   {
     // Only if this is a master image and copyright statement is enabled
-    if (QubitTerm::MASTER_ID != $this->usageId || !sfConfig::get('app_digitalobject_copyright_statement_enabled', false))
-    {
+    if (QubitTerm::MASTER_ID != $this->usageId
+      || !sfConfig::get('app_digitalobject_copyright_statement_enabled', false)
+    ) {
       return false;
     }
 