e08ee7918cd657f782de563f96e66a75968dc2e4
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-05-24 14:57:05 +0200

    Do not use magick command.

--- plugins/sfThumbnailPlugin/lib/sfImageMagickAdapter.class.php
+++ plugins/sfThumbnailPlugin/lib/sfImageMagickAdapter.class.php
@@ -135,7 +135,7 @@ class sfImageMagickAdapter
     public function __construct($maxWidth, $maxHeight, $scale, $inflate, $quality, $options)
     {
         $this->magickCommands = [];
-        $this->magickCommands['convert'] = isset($options['convert']) ? escapeshellcmd($options['convert']) : 'magick convert';
+        $this->magickCommands['convert'] = isset($options['convert']) ? escapeshellcmd($options['convert']) : 'convert';
         $this->magickCommands['identify'] = isset($options['identify']) ? escapeshellcmd($options['identify']) : 'identify';
 
         exec($this->magickCommands['convert'] . ' -version', $stdout);
