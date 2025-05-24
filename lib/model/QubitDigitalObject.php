a7474815fa36d467e656f722f2046890ad5ceaf1
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-05-24 14:57:16 +0200

    Report any error.

--- lib/model/QubitDigitalObject.php
+++ lib/model/QubitDigitalObject.php
@@ -2017,8 +2017,8 @@ class QubitDigitalObject extends BaseDigitalObject
             $command .= ' '.$filenameMinusExtension.'_%02d.'.self::THUMB_EXTENSION;
             exec($command, $output, $status);
 
-            if (1 == $status) {
-                throw new sfException('Encountered error'.(is_array($output) && count($output) > 0 ? ': '.implode('\n'.$output) : ' ').' while running convert (ImageMagick).');
+            if (0 != $status) {
+                throw new sfException('Encountered error'.(is_array($output) && count($output) > 0 ? ": $status ".implode('\n'.$output) : ' ').' while running convert (ImageMagick).');
             }
 
             // Build an array of the exploded file names
