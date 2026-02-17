63515617ede02cb0d9248f013749344953d18c8d
Orestes Sanchez Benavente <orestes.sanchezbenavente@telefonica.com>
2026-02-17 17:25:48 +0100

    Update queries

--- docs/term-queries.sql
+++ docs/term-queries.sql
@@ -2,18 +2,16 @@
 -- QubitObjectTermRelation::TABLE_NAME = object_term_relation
 -- QubitInformationObject::TABLE_NAME = information_object
 
--- Report on the usabes of taxonomy terms in information objects.
+-- Report on the usages of taxonomy terms in information objects.
 -- This is a simple report that counts the number of times each term is
 -- used in information objects. The report is ordered by term id.
 
-SELECT DISTINCT t.id, tin.name, COUNT(i.id) AS use_count
-FROM term t
-         INNER JOIN object_term_relation r
-                    ON r.term_id = t.id
-         INNER JOIN information_object i ON r.object_id = i.id
-         inner join term_i18n tin on t.id = tin.id
-WHERE t.taxonomy_id = 35
-  and tin.culture = 'ca'
+SELECT DISTINCT t.id, tin.name, s.slug, COUNT(i.id) AS use_count
+FROM term t INNER JOIN object_term_relation r
+                       ON r.term_id=t.id INNER JOIN information_object i ON r.object_id=i.id
+            inner join term_i18n tin on t.id=tin.id
+            inner join slug s on t.id = s.object_id
+WHERE t.taxonomy_id=35 and tin.culture ='ca'
 GROUP BY (t.id)
 ORDER BY 3 desc
 
@@ -48,11 +46,30 @@ from slug
 where slug = :slug
 
 -- obtener el term id de una cultura y slug
-select t.id, tin.name, tin.culture
+select t.id, tin.name , tin.culture
 from term t
          inner join term_i18n tin on t.id = tin.id
          inner join slug s on s.object_id = t.id
-where s.slug = :slug -- 'actes-culturals-3'
+where s.slug=:slug -- 'actes-culturals-3'
+  and culture =:culture -- 'ca'
+
+-- obtener el term id de una cultura y nombre de termino
+select t.id, s.slug, tin.name, tin.culture
+from term t
+         inner join term_i18n tin on t.id = tin.id
+         inner join slug s on s.object_id = t.id
+where tin.name COLLATE utf8mb4_general_ci  = :name -- 'Actes culturals'
   and culture = :culture -- 'ca'
 
 
+
+update object_term_relation
+    FROM term t
+    INNER JOIN object_term_relation r
+on r.term_id = t.id
+    inner join term_i18n tin on t.id = tin.id
+    INNER JOIN information_object i ON r.object_id = i.id
+    INNER join information_object_i18n ioin on ioin.id = i.id and tin.culture = ioin.culture
+set term_id = :new_term_id
+WHERE t.id = :old_term_id -- 52136
+  and tin.culture = :culture
